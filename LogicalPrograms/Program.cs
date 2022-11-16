7451bb17028f26d7732c0908754427d9f8b761b7
akshata_rn <akshatarn123@gamail.com>
2022-11-16 15:05:12 +0530

    Fibonacci Series Program

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,6 +11,20 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Fibonacci Program");
+            Console.WriteLine("Enter the number of elements:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1,n3;
+            Console.Write(n1 + " " + n2 + " ");
+            for(int i = 2; i< n; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+            Console.ReadLine();
         }
     }
 }
